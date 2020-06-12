febfb6a734b4cf89808fea52e4cc296d6bc36b5e
nylachoirotun <31815400+nylachoirotun@users.noreply.github.com>
2020-06-13 04:42:27 +0700

    Create pdf.php

---
+++ application/views/member/laporan/pdf.php
@@ -0,0 +1,110 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!doctype html>
+<html class="fixed sidebar-left-collapsed">
+<head>  
+  <meta charset="UTF-8"> 
+  <link rel="shortcut icon" href="<?php echo base_url()?>/assets/images/fav.png" type="image/ico">   
+  <title>PT Argopuro</title>    
+  <meta name="author" content="Paber">   
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/vendor/bootstrap/css/bootstrap.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/vendor/font-awesome/css/font-awesome.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/vendor/magnific-popup/magnific-popup.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/vendor/bootstrap-datepicker/css/datepicker3.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/vendor/select2/select2.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/vendor/jquery-datatables-bs3/assets/css/datatables.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/stylesheets/theme.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/stylesheets/skins/default.css" />
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/stylesheets/theme-custom.css">
+  <link rel="stylesheet" href="<?php echo base_url()?>/assets/stylesheets/admin.min.css">
+  <link rel="stylesheet" href="<?php echo base_url()?>assets/vendor/pnotify/pnotify.custom.css" />
+
+
+  <!-- Head Libs -->
+  <script src="<?php echo base_url()?>/assets/vendor/modernizr/modernizr.js"></script>
+</head>
+<body class="bgbody">
+  <section class="body">
+
+     <?php $this->load->view("komponen/header.php") ?>
+     <div class="inner-wrapper"> 
+        <?php $this->load->view("komponen/sidebar.php") ?>
+        <section role="main" class="content-body">
+           <header class="page-header">  
+              <h2>Master Data Tanah</h2>
+          </header>  
+          <!-- start: page -->
+          <section class="panel">
+            <header class="panel-heading">    
+                <div class="row show-grid">
+                    <div class="col-md-6" align="left"><h2 class="panel-title">Data Tanah </h2></div>
+                    <?php  
+                    echo level_user('master','items',$this->session->userdata('kategori'),'add') > 0 ? '<div class="col-md-6" align="right"><a class="btn btn-success" href="#"  data-toggle="modal" data-target="#tambahData"><i class="fa fa-plus"></i> Tambah</a></div>':'';
+                    ?> 
+                </div>
+            </header>
+            <div class="panel-body"> 
+                  <div >
+                          <table class="table table-bordered table-hover table-striped" id="itemsdata">
+                        <thead>
+                            <tr>
+                              
+                                <th rowspan="2" style="text-align: center;">ID Penjualan</th>
+                                <th rowspan="2" style="text-align: center;">Tanggal</th>
+                                <th rowspan="2" style="text-align: center;">Total</th>
+                                <th rowspan="2" style="text-align: center;">Nama User</th>
+                                <th rowspan="2" style="text-align: center;">Jenis Penjualan</th>
+                                <th colspan="2" style="text-align: center;">Jenis Pembayaran</th>
+                                <th rowspan="2" style="text-align: center;">No Kartu Debit</th>
+           
+
+           
+                    </tr>
+                 
+                    
+                </thead>
+                <tbody>
+                    <td> 1 </td>
+                    <td>08-03-2020  </td>
+                    <td>20.000</td>
+                    <td>100.000</td>
+                    <td>GOJEK</td>
+                    <td>NON TUNAI</td>
+                    <td>12345565757 gopay</td>
+                </tbody>
+            </table> 
+            </div></div>
+        </section>
+        <!-- end: page -->
+    </section>
+</div>
+</section>
+
+
+
+
+
+</div>  
+
+<!-- Vendor -->
+<script src="<?php echo base_url()?>assets/vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/jquery-browser-mobile/jquery.browser.mobile.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/bootstrap/js/bootstrap.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/nanoscroller/nanoscroller.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/magnific-popup/magnific-popup.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/jquery-placeholder/jquery.placeholder.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/select2/select2.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/jquery-datatables/media/js/jquery.dataTables.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/jquery-datatables-bs3/assets/js/datatables.js"></script>
+<script src="<?php echo base_url()?>assets/javascripts/theme.js"></script>
+<script src="<?php echo base_url()?>assets/javascripts/admin.min.js"></script>
+<script src="<?php echo base_url()?>assets/vendor/pnotify/pnotify.custom.js"></script>
+<script src="<?php echo base_url()?>assets/javascripts/theme.init.js"></script> 
+<script type="text/javascript">  
+
+
+</script>
+</body>
+</html>
